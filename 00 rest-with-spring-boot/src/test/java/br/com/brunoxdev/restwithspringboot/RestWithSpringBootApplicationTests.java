d178939304a36c0c8ce63538ec845dd9a851c8bb
Bruno Henrique <bruno.this.developer@gmail.com>
2021-04-10 13:02:41 -0300

    Hello SpringBoot and REST Udemy

---
+++ 00 rest-with-spring-boot/src/test/java/br/com/brunoxdev/restwithspringboot/RestWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunoxdev.restwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ 01 rest-with-spring-boot scaffold Eclipse/src/main/java/br/com/bruno/Startup.java
@@ -0,0 +1,15 @@
+package br.com.bruno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+	public static void main(String[] args) {
+		
+		SpringApplication.run(Startup.class, args);
+		
+
+		
+	}
+}
